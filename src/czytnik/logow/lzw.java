23a07f13d9d551ddf3dc1daa8f6ce15fc156a94b
Wojciech <Wojciech@Wojciech-LapWIN>
2014-12-20 12:31:24 +0100

    Strat projektu.

---
+++ src/czytnik/logow/lzw.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package czytnik.logow;
+
+/**
+ *
+ * @author Wojciech
+ */
+public class lzw implements AlgorytmKompresji{
+
+    @Override
+    public byte[] kompresuj(String dane) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String dekompresuj(byte[] daneSkompresowane) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
